126a4d373ed898fe4320fefcbb215d65b057f5cb
czam20 <cc.zambrano22@gmail.com>
2021-03-16 12:54:31 -0400

    Agregue estilos a las tablas

--- resources/views/programacion/index.blade.php
+++ resources/views/programacion/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
 
-    <div class="container bg-red-500">
+    <div class="card bg-gray-dark">
         <form action="{{route('programacion.mostrar')}}" method="GET">
             <input type="date" value="{{date("Y-m-d")}}" name="fecha"></input>
             <button type="submit">Enviar</button>
